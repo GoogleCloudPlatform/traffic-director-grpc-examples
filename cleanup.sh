95feb083d5a4d31b8cc75fed980d00e0b3d58631
Menghan Li <menghanl@google.com>
2020-07-31 15:38:14 -0700

    add cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+# TD resources
+gcloud compute forwarding-rules delete grpcwallet-forwarding-rule --global -q
+gcloud compute target-grpc-proxies delete grpcwallet-proxy -q
+gcloud compute url-maps delete grpcwallet-url-map -q
+
+# per service
+services=(
+    "grpcwallet-account"
+    "grpcwallet-stats"
+    "grpcwallet-stats-premium"
+    "grpcwallet-wallet-v1"
+    "grpcwallet-wallet-v2"
+)
+for s in "${services[@]}"; do
+    gcloud compute backend-services delete "$s"-service --global -q
+    gcloud compute instance-groups managed delete "$s"-mig-us-central1 --zone us-central1-a -q
+    gcloud compute instance-templates delete "$s"-template -q
+done
+
+# health check & firewall
+gcloud compute firewall-rules delete grpcwallet-allow-health-checks -q
+gcloud compute health-checks delete grpcwallet-health-check -q
