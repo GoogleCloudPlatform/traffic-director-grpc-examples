5cc8baa131035be6eb0ca72bb5c9de560591e091
Menghan Li <menghanl@google.com>
2020-08-04 18:14:28 -0700

    all

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -x
+
+# $1 = language, one of "java" or "go"
+if ! [[ $1 =~ ^(go|java)$ ]]; then
+    echo "language $1 is undefined, pick one from [go, java]"
+    exit 123
+fi
+
 ./create_health_check.sh
 ./create_service.sh $1 account 50053 account
 ./create_service.sh $1 stats   50052 stats         '--account_server="xds:///account.grpcwallet.io"'
