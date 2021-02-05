4dd1e2bc9d4bdf6dddbe7ac2747ee68c2968c779
Eric Gribkoff <ericgribkoff@google.com>
2021-02-04 17:30:58 -0800

    add comments, style fixes

--- java/src/test/java/io/grpc/examples/wallet/ObservabilityTest.java
+++ java/src/test/java/io/grpc/examples/wallet/ObservabilityTest.java
@@ -2,6 +2,7 @@ package io.grpc.examples.wallet;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.collect.ImmutableSet;
 import io.grpc.ManagedChannel;
 import io.grpc.Server;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
@@ -20,7 +21,6 @@ import io.opencensus.trace.config.TraceConfig;
 import io.opencensus.trace.export.SpanData;
 import io.opencensus.trace.export.SpanExporter;
 import io.opencensus.trace.samplers.Samplers;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -29,31 +29,76 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class ObservabilityTest {
 
   private static final int EXPORT_WAIT_SEC = 15;
 
   private static final Set<String> METRICS =
-      new HashSet<>(
-          Arrays.asList(
-              "grpc.io/client/roundtrip_latency",
-              "grpc.io/client/completed_rpcs",
-              "grpc.io/client/sent_bytes_per_rpc",
-              "grpc.io/client/received_bytes_per_rpc",
-              "grpc.io/client/sent_messages_per_rpc",
-              "grpc.io/client/received_messages_per_rpc",
-              "grpc.io/server/completed_rpcs",
-              "grpc.io/server/sent_bytes_per_rpc",
-              "grpc.io/server/received_bytes_per_rpc",
-              "grpc.io/server/sent_messages_per_rpc",
-              "grpc.io/server/received_messages_per_rpc",
-              "grpc.io/server/server_latency"));
+      ImmutableSet.of(
+          "grpc.io/client/roundtrip_latency",
+          "grpc.io/client/completed_rpcs",
+          "grpc.io/client/sent_bytes_per_rpc",
+          "grpc.io/client/received_bytes_per_rpc",
+          "grpc.io/client/sent_messages_per_rpc",
+          "grpc.io/client/received_messages_per_rpc",
+          "grpc.io/server/completed_rpcs",
+          "grpc.io/server/sent_bytes_per_rpc",
+          "grpc.io/server/received_bytes_per_rpc",
+          "grpc.io/server/sent_messages_per_rpc",
+          "grpc.io/server/received_messages_per_rpc",
+          "grpc.io/server/server_latency");
 
   private Server server;
   private ManagedChannel channel;
   private RecordingMetricsExporter recordingMetricsExporter;
   private RecordingTraceExporter recordingTraceExporter;
+
+  @Before
+  public void setUp() throws Exception {
+    RpcViews.registerAllGrpcViews();
+    recordingMetricsExporter = new RecordingMetricsExporter();
+
+    TraceConfig traceConfig = Tracing.getTraceConfig();
+    traceConfig.updateActiveTraceParams(
+        traceConfig.getActiveTraceParams().toBuilder().setSampler(Samplers.alwaysSample()).build());
+    recordingTraceExporter = new RecordingTraceExporter();
+    Tracing.getExportComponent()
+        .getSpanExporter()
+        .registerHandler(RecordingTraceExporter.class.getName(), recordingTraceExporter);
+
+    server = NettyServerBuilder.forPort(0).addService(new WalletImpl()).build();
+    server.start();
+    channel = NettyChannelBuilder.forAddress("localhost", server.getPort()).usePlaintext().build();
+  }
+
+  @After
+  public void tearDown() {
+    channel.shutdownNow();
+    server.shutdownNow();
+  }
+
+  @Test
+  public void testMetrics() throws Exception {
+    WalletGrpc.newBlockingStub(channel).fetchBalance(BalanceRequest.getDefaultInstance());
+    recordingMetricsExporter.latch.await(EXPORT_WAIT_SEC, TimeUnit.SECONDS);
+    assertThat(recordingMetricsExporter.metrics).containsAtLeastElementsIn(METRICS);
+  }
+
+  @Test
+  public void testTrace() throws Exception {
+    WalletGrpc.newBlockingStub(channel).fetchBalance(BalanceRequest.getDefaultInstance());
+    recordingTraceExporter.latch.await(EXPORT_WAIT_SEC, TimeUnit.SECONDS);
+    assertThat(recordingTraceExporter.clientSpanData).isNotNull();
+    assertThat(recordingTraceExporter.serverSpanData).isNotNull();
+    assertThat(recordingTraceExporter.serverSpanData.getContext().getTraceId())
+        .isEqualTo(recordingTraceExporter.clientSpanData.getContext().getTraceId());
+    assertThat(recordingTraceExporter.serverSpanData.getParentSpanId())
+        .isEqualTo(recordingTraceExporter.clientSpanData.getContext().getSpanId());
+  }
 
   private static class WalletImpl extends WalletGrpc.WalletImplBase {
     @Override
@@ -65,7 +110,9 @@ public class ObservabilityTest {
   }
 
   private class RecordingTraceExporter extends SpanExporter.Handler {
+    // The trace span exported by the client (identified by a null parent span)
     SpanData clientSpanData = null;
+    // The trace span exported by the server (identified by a non-null parent span)
     SpanData serverSpanData = null;
     CountDownLatch latch = new CountDownLatch(2);
 
@@ -111,51 +158,5 @@ public class ObservabilityTest {
         latch.countDown();
       }
     }
-  }
-
-  @Before
-  public void setUp() throws Exception {
-    RpcViews.registerAllGrpcViews();
-    recordingMetricsExporter = new RecordingMetricsExporter();
-
-    TraceConfig traceConfig = Tracing.getTraceConfig();
-    traceConfig.updateActiveTraceParams(
-        traceConfig.getActiveTraceParams().toBuilder().setSampler(Samplers.alwaysSample()).build());
-    recordingTraceExporter = new RecordingTraceExporter();
-    Tracing.getExportComponent()
-        .getSpanExporter()
-        .registerHandler(RecordingTraceExporter.class.getName(), recordingTraceExporter);
-
-    server = NettyServerBuilder.forPort(0).addService(new WalletImpl()).build();
-    server.start();
-    channel = NettyChannelBuilder.forAddress("localhost", server.getPort()).usePlaintext().build();
-  }
-
-  @After
-  public void tearDown() {
-    channel.shutdownNow();
-    server.shutdownNow();
-  }
-
-  @Test
-  public void testMetrics() throws Exception {
-    WalletGrpc.newBlockingStub(channel).fetchBalance(BalanceRequest.getDefaultInstance());
-
-    recordingMetricsExporter.latch.await(EXPORT_WAIT_SEC, TimeUnit.SECONDS);
-    assertThat(recordingMetricsExporter.metrics).containsAtLeastElementsIn(METRICS);
-  }
-
-  @Test
-  public void testTrace() throws Exception {
-    WalletGrpc.newBlockingStub(channel).fetchBalance(BalanceRequest.getDefaultInstance());
-
-    recordingTraceExporter.latch.await(EXPORT_WAIT_SEC, TimeUnit.SECONDS);
-
-    assertThat(recordingTraceExporter.clientSpanData).isNotNull();
-    assertThat(recordingTraceExporter.serverSpanData).isNotNull();
-    assertThat(recordingTraceExporter.serverSpanData.getContext().getTraceId())
-        .isEqualTo(recordingTraceExporter.clientSpanData.getContext().getTraceId());
-    assertThat(recordingTraceExporter.serverSpanData.getParentSpanId())
-        .isEqualTo(recordingTraceExporter.clientSpanData.getContext().getSpanId());
   }
 }
