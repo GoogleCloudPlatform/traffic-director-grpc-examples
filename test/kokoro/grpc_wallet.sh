c221ebedb23153d110d23d765fe06b76919b08d5
Lidi Zheng <lidiz@google.com>
2021-05-18 17:27:50 -0700

    Just an update

--- test/kokoro/grpc_wallet.sh
+++ test/kokoro/grpc_wallet.sh
@@ -7,6 +7,8 @@ fi
 
 cd github/traffic-director-grpc-examples/scripts
 
+sudo apt-get update && sudo apt-get --only-upgrade install google-cloud-sdk-datalab google-cloud-sdk-anthos-auth google-cloud-sdk-bigtable-emulator google-cloud-sdk-pubsub-emulator google-cloud-sdk-skaffold google-cloud-sdk-firestore-emulator google-cloud-sdk-cloud-build-local google-cloud-sdk google-cloud-sdk-kubectl-oidc google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-python-extras google-cloud-sdk-spanner-emulator google-cloud-sdk-config-connector google-cloud-sdk-minikube google-cloud-sdk-app-engine-grpc google-cloud-sdk-app-engine-java kubectl google-cloud-sdk-datastore-emulator google-cloud-sdk-app-engine-go google-cloud-sdk-cbt google-cloud-sdk-local-extract google-cloud-sdk-kpt
+
 ./all.sh go
 ./cleanup.sh
 
