8655d4d6cfcec7d81be760c02166bbeb87753a4a
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2021-10-06 11:26:20 -0700

    wallet/gke-scripts remove beta from all gcloud commands

--- gke-scripts/70-security-components.sh
+++ gke-scripts/70-security-components.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 function create_security_components {
-  envsubst < ServerMtlsPolicy.yaml | gcloud beta network-security \
+  envsubst < ServerMtlsPolicy.yaml | gcloud network-security \
     server-tls-policies import ${SERVER_MTLS_POLICY_NAME} --location=global
 
-  envsubst < EndpointPolicy.yaml | gcloud beta network-services \
+  envsubst < EndpointPolicy.yaml | gcloud network-services \
     endpoint-policies import ${ENDPOINT_MTLS_POLICY_NAME} --location=global
 
-  envsubst < ClientMtlsPolicy.yaml | gcloud beta network-security \
+  envsubst < ClientMtlsPolicy.yaml | gcloud network-security \
     client-tls-policies import ${CLIENT_MTLS_POLICY_NAME} --location=global
 
   backend_services=("${ACCOUNT_BACKEND_SERVICE_NAME}" \
@@ -17,23 +17,23 @@ function create_security_components {
     "${WALLET_V2_BACKEND_SERVICE_NAME}")
   for bs in ${backend_services[@]}
   do
-    gcloud beta compute backend-services export ${bs} --global \
+    gcloud compute backend-services export ${bs} --global \
       --destination=/tmp/${bs}.yaml
 
     envsubst < ClientSecuritySettings.yaml | cat /tmp/${bs}.yaml - \
       >/tmp/${bs}-new.yaml
 
-    gcloud beta compute backend-services import ${bs} --global \
+    gcloud compute backend-services import ${bs} --global \
       --source=/tmp/${bs}-new.yaml -q
   done
 }
 
 
 function delete_server_security_components {
-  gcloud beta network-services endpoint-policies delete ${ENDPOINT_MTLS_POLICY_NAME} --location=global -q
-  gcloud beta network-security server-tls-policies delete ${SERVER_MTLS_POLICY_NAME} --location=global -q
+  gcloud network-services endpoint-policies delete ${ENDPOINT_MTLS_POLICY_NAME} --location=global -q
+  gcloud network-security server-tls-policies delete ${SERVER_MTLS_POLICY_NAME} --location=global -q
 }
 
 function delete_client_security_components {
-  gcloud beta network-security client-tls-policies delete ${CLIENT_MTLS_POLICY_NAME} --location=global -q
+  gcloud network-security client-tls-policies delete ${CLIENT_MTLS_POLICY_NAME} --location=global -q
 }
